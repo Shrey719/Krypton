beedcfca8afd2ae8b0528811abf4181eaeee17a3
Shrey719 <shrey150436@gmail.com>
2024-11-01 18:34:26 -0400

    commiting because im about to do some things with my comp

--- static/assets/js/linux/installlinux.js
+++ static/assets/js/linux/installlinux.js
@@ -1,15 +1,37 @@
-let fs = JSON.parse(localStorage.getItem("fs")) // yes, this will interact directly with the fs (no wrappers)
+ fs = JSON.parse(localStorage.getItem("fs")) // yes, this will interact directly with the fs (no wrappers)
 
-fs.bin.bash["mkdir"] = {}
-fs.bin.bash.mkdir.main = function(str) {
+    fs.bin.bash["mkdir"] = {}
+        // so the way this works is mkdir(home/usr/example/folder)
+    fs.bin.bash.mkdir.main = function(str) {
+            let obj = JSON.parse(localStorage.getItem("fs"))
+            const keys = str.split("/");
+
+            let current = obj;
+            for (let i = 0; i < keys.length; i++) {
+                const key = keys[i];
+                if (i === keys.length - 1) {
+                    current[key] = {}; 
+                } else {
+                    if (!current[key]) {
+                        current[key] = {};
+                    }
+                    current = current[key]; 
+                }
+            }
+            localStorage.setItem("fs", JSON.stringify(obj)) 
+
+        }
+
+fs.bin.bash["touch"] = {}
+fs.bin.bash.touch.main = function(loc) {
     let obj = JSON.parse(localStorage.getItem("fs"))
-    const keys = str.split(".");
+    const keys = loc.split("/");
 
     let current = obj;
     for (let i = 0; i < keys.length; i++) {
         const key = keys[i];
         if (i === keys.length - 1) {
-            current[key] = {}; 
+            current[key] = ""; 
         } else {
             if (!current[key]) {
                 current[key] = {};
@@ -17,6 +39,8 @@ fs.bin.bash.mkdir.main = function(str) {
             current = current[key]; 
         }
     }
-    return obj
+    localStorage.setItem("fs", JSON.stringify(obj)) 
 
 }
+
+delete fs // manual things (and also no more conflicts  )
