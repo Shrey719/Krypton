939fd47e495fe810d7b49503cac9f04925bc29cf
Shrey719 <shrey150436@gmail.com>
2024-11-10 17:32:17 -0500

    if this breaks smth istg

--- static/assets/js/system/sysflags.js
+++ static/assets/js/system/sysflags.js
@@ -5,3 +5,5 @@ rootfs.sys.systemFlags = {
 	allowRootfsEditing: false, // rootfs write protected by default
 	rescueModeOnRootFSCorruption: true // if something happens to the rootfs, kick back to the oobe/rescue mode;
 }
+
+localStorage.setItem("fs", JSON.stringify(rootfs))
