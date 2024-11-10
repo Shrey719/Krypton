daba11cf91d096c4d806b513c034b7ecf6768dc3
Shrey719 <shrey150436@gmail.com>
2024-11-10 17:29:35 -0500

    i have brain damage

--- static/assets/js/system/sysflags.js
+++ static/assets/js/system/sysflags.js
@@ -3,5 +3,5 @@ let rootfs = JSON.parse(localStorage.getItem("fs"))
 rootfs.sys.systemFlags = {
 	allowJavascriptTerm: false, // no js term 
 	allowRootfsEditing: false, // rootfs write protected by default
-	rescueModeOnRootFSCorruption: true; // if something happens to the rootfs, kick back to the oobe/rescue mode;
+	rescueModeOnRootFSCorruption: true // if something happens to the rootfs, kick back to the oobe/rescue mode;
 }
