e88acd48d9c6734c817d4e8fd8bf006c0efc743a
Shrey719 <shrey150436@gmail.com>
2024-11-10 17:43:58 -0500

    created system flags and put in OOBE

--- static/assets/js/system/sysflags.js
+++ static/assets/js/system/sysflags.js
@@ -1,9 +1,22 @@
 let rootfs = JSON.parse(localStorage.getItem("fs"))
 
+function setDevmodeDisabled() {
 rootfs.sys.systemFlags = {
 	allowJavascriptTerm: false, // no js term 
 	allowRootfsEditing: false, // rootfs write protected by default
-	rescueModeOnRootFSCorruption: true // if something happens to the rootfs, kick back to the oobe/rescue mode;
+	rescueModeOnRootFSCor: false // if something happens to the rootfs, kick back to the oobe
 }
 
 localStorage.setItem("fs", JSON.stringify(rootfs))
+}
+
+function setDevmodeEnabled() {
+
+rootfs.sys.systemFlags = {
+	allowJavascriptTerm: true,
+	allowRootfsEditing: true, // people with the ability to edit the rootfs CAN toggle this flag/change the flag in any way they want
+	rescueModeOnRootFSCor: true // if something happens to the rootfs, kick to rescue mode
+}
+
+localStorage.setItem("fs", JSON.stringify(rootfs))
+}
