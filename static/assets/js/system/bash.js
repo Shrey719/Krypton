fd032116dbef5f30b20be2a3c84dadf51da02906
Shrey719 <shrey150436@gmail.com>
2024-11-10 19:41:02 -0500

    fix formatting & error codes

--- static/assets/js/system/bash.js
+++ static/assets/js/system/bash.js
@@ -6,18 +6,17 @@ function printuf(input) {
 }
 
 function sysfetch() {
-	try { 
-		let avalibleCores =  navigator.hardwareConcurrency;
-		let osver = localStorage.getItem("version")
-		
-	} catch(e) {
+	try {
+	let avalibleCores = navigator.hardwareConcurrency;
+	let osver = localStorage.getItem("version")
 		return {
-			EXIT_CODE: 1,
-			OUTPUT: `Error in fetching system contents`
-		}
-	}
-	return {
 		EXIT_CODE: 0, 
-		OUTPUT: `Avalible cores: ${avlibleCores} \n Krypton Version: ${osver}`
+		OUTPUT: `Avalible cores: ${avalibleCores}\n Krypton Version: ${osver}`
+	}
+	} catch (e) {
+		return {
+		EXIT_CODE: 1,
+		OUTPUT: "Error in fetching system " + e
+		}
 	}
 }
